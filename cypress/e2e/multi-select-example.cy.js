b0e922d8ef704b5f3a314e00758b294a29cebddc
RanjithKumarKarnan <136866781+RanjithKumarKarnan@users.noreply.github.com>
2023-10-20 22:20:16 +0530

    Add files via upload

---
+++ cypress/e2e/multi-select-example.cy.js
@@ -0,0 +1,43 @@
+describe("the multi select example", () => {
+  it("allows multiple selections", () => {
+    cy.visit("/multi-select-example/");
+    cy.get("much-select").click();
+    cy.get("much-select")
+      .shadow()
+      .find("#dropdown")
+      .find("[data-value='Java']")
+      .click();
+    cy.get("much-select")
+      .shadow()
+      .find("#dropdown")
+      .find("[data-value='Go']")
+      .click();
+    cy.get("much-select").should("have.attr", "selected-value", "Java,Go");
+  });
+  it("filters the dropdown based on what the user types", () => {
+    cy.visit("/multi-select-example/");
+    cy.get("much-select").click();
+    cy.get("much-select")
+      .shadow()
+      .find("#input-filter")
+      .click()
+      .wait(1000)
+      .type("jav");
+    cy.get("much-select")
+      .shadow()
+      .find("#dropdown")
+      .children()
+      .should("have.attr", "data-value", "Java");
+  });
+  it("the first item in the dropdown should be highlighted", () => {
+    cy.visit("/multi-select-example/");
+    cy.get("much-select").click();
+    cy.get("much-select").shadow().find("#input-filter").click().type("al");
+    cy.get("much-select")
+      .shadow()
+      .find("#dropdown")
+      .children()
+      .first()
+      .should("have.class", "highlighted");
+  });
+});
